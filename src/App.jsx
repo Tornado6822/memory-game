d063c94db3036ad332d5ece781b5c406d5f8a678
Tornado6822 <morelpatrick97@gmail.com>
2026-06-19 22:05:28 -0400

    Added basic page styling, grid sketch

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,51 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container mt-5 p-5 d-flex justify-content-center">
+        <div id="grid">
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div
+            className="square"
+            style={{ backgroundColor: "var(--primary)" }}
+          ></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div
+            className="square"
+            style={{ backgroundColor: "var(--primary)" }}
+          ></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div
+            className="square"
+            style={{ backgroundColor: "var(--primary)" }}
+          ></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div className="square"></div>
+          <div
+            className="square"
+            style={{ backgroundColor: "var(--primary)" }}
+          ></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
