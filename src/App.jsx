ca6e0b3921f66e10a8bfd87f4e5d1635043f09b7
Tornado6822 <morelpatrick97@gmail.com>
2026-06-20 21:12:02 -0400

    generateLevel function implemented.

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,33 @@ function App() {
   const [board, setBoard] = useState(Array(25).fill(false));
   const [level, setLevel] = useState(0);
 
+  function generateLevel() {
+    const numToSelect = level * 3 + 5;
+    let selected = [];
+    for (let i = 0; i < numToSelect; i++) {
+      let selectedIndex = Math.floor(Math.random() * board.length);
+      while (selected.includes(selectedIndex)) {
+        selectedIndex = Math.floor(Math.random() * board.length);
+      }
+      selected.push(selectedIndex);
+    }
+
+    setBoard((prev) => {
+      const newBoard = [...prev];
+      selected.forEach((index) => {
+        newBoard[index] = true;
+      });
+      return newBoard;
+    });
+  }
+
   function handleClick(index) {
     setBoard((prev) => {
       const newBoard = [...prev];
       newBoard[index] = !newBoard[index];
       return newBoard;
     });
+    generateLevel();
   }
 
   return gameStarted ? (
--- src/components/Gameboard.jsx
+++ src/components/Gameboard.jsx
@@ -2,8 +2,6 @@ import Grid from "./Grid";
 import "./Gameboard.css";
 
 function Gameboard({ board, handleClick }) {
-  console.log(board);
-
   return (
     <div className="container my-container mt-5 p-5 d-flex justify-content-center">
       <Grid board={board} handleClick={handleClick} />
